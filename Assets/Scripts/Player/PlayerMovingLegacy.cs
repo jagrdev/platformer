3952ac69b578970d4e60b883562fa6b45a6a69c7
Sizykh Nick <nksnick@gmail.com>
2022-04-23 23:07:50 +1000

    Сделал устаревшее перемещение на кнопки

---
+++ Assets/Scripts/Player/PlayerMovingLegacy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerMovingLegacy : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private float _speed = 3.0f;
+
+    /// <summary>
+   /// Update is called every frame, if the MonoBehaviour is enabled.
+   /// </summary>
+   void Update()
+   {
+       if (Input.GetKey(KeyCode.A))
+       {
+           var position = player.transform.position;
+           player.transform.position = new Vector3(position.x - Time.deltaTime * _speed, position.y);
+       }
+       else if (Input.GetKey(KeyCode.D))
+       {
+           var position = player.transform.position;
+           player.transform.position = new Vector3(position.x + Time.deltaTime * _speed, position.y);
+       }
+       
+   }
+}
