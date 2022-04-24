41521c0dd99f321f116307de5bf95b40b6529c04
Sizykh Nick <nksnick@gmail.com>
2022-04-24 13:39:49 +1000

    Переделал перемещение на нормальный Vector2
    Дало возможность избавиться на стороне обработчика колбеков

--- Assets/Scripts/Player/PlayerActionReader.cs
+++ Assets/Scripts/Player/PlayerActionReader.cs
@@ -4,27 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerActionReader : MonoBehaviour
 {
     [SerializeField] private Hero character;
-    private Vector3 _motion;
 
     /// <summary>
     /// Перемещает игрока в нужном направлении
     /// </summary>
     /// <param name="context">Содержит информацию о вводе пользователя</param>
-    public void OnHorizontalMotion(InputAction.CallbackContext context)
+    public void OnMotion(InputAction.CallbackContext context)
     {
-        var delta = context.ReadValue<float>();
-        _motion = new Vector3(delta, _motion.y);
-        character.Move(_motion);
-    }
-    
-    /// <summary>
-    /// Перемещает игрока в нужном направлении
-    /// </summary>
-    /// <param name="context">Содержит информацию о вводе пользователя</param>
-    public void OnVerticalMotion(InputAction.CallbackContext context)
-    {
-        var delta = context.ReadValue<float>();
-        _motion = new Vector3( _motion.x, delta);
-        character.Move(_motion);
+        var delta = context.ReadValue<Vector2>();
+        character.Move(delta);
     }
 }
