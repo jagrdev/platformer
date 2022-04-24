2288839b898e66df240175fd31cafd0bf37055a5
Sizykh Nick <nksnick@gmail.com>
2022-04-24 12:07:09 +1000

    Переделал движение на новую систему ввода

---
+++ Assets/Scripts/Player/Hero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+/// <summary>
+/// Описывает поведение главного персонажа
+/// </summary>
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed = 3.0f;
+    private float _motion = 0f;
+
+    /// <summary>
+    /// Перемещение по горизонтали
+    /// </summary>
+    /// <param name="motion">Отрицательное значение двигает влево, положительное вправо</param>
+    public void Move(float motion)
+    {
+        _motion = motion;
+    }
+
+    private void Update()
+    {
+        var position = player.transform.position;
+        player.transform.position = new Vector3(position.x + _motion * speed * Time.deltaTime, position.y);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerActionReader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerActionReader : MonoBehaviour
+{
+    [SerializeField] private Hero player;
+
+    /// <summary>
+    /// Перемещает игрока в нужном направлении
+    /// </summary>
+    /// <param name="context">Содержит информацию о вводе пользователя</param>
+    public void OnPlayerMotion(InputAction.CallbackContext context)
+    {
+        var motion = context.ReadValue<float>();
+        player.Move(motion);
+    }
+}
