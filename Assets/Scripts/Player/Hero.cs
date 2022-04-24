da9017c7209ccfb2a3a1fa929b9274e57e4b7595
Sizykh Nick <nksnick@gmail.com>
2022-04-24 12:49:16 +1000

    Добавил вертикальное перемещение

--- Assets/Scripts/Player/Hero.cs
+++ Assets/Scripts/Player/Hero.cs
@@ -9,20 +9,19 @@ public class Hero : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private float speed = 3.0f;
-    private float _motion = 0f;
+    private Vector3 _motion;
 
     /// <summary>
     /// Перемещение по горизонтали
     /// </summary>
     /// <param name="motion">Отрицательное значение двигает влево, положительное вправо</param>
-    public void Move(float motion)
+    public void Move(Vector3 motion)
     {
         _motion = motion;
     }
 
     private void Update()
     {
-        var position = player.transform.position;
-        player.transform.position = new Vector3(position.x + _motion * speed * Time.deltaTime, position.y);
+        player.transform.position += _motion * (speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Player/PlayerActionReader.cs
+++ Assets/Scripts/Player/PlayerActionReader.cs
@@ -3,15 +3,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerActionReader : MonoBehaviour
 {
-    [SerializeField] private Hero player;
+    [SerializeField] private Hero character;
+    private Vector3 _motion;
 
     /// <summary>
     /// Перемещает игрока в нужном направлении
     /// </summary>
     /// <param name="context">Содержит информацию о вводе пользователя</param>
-    public void OnPlayerMotion(InputAction.CallbackContext context)
+    public void OnHorizontalMotion(InputAction.CallbackContext context)
     {
-        var motion = context.ReadValue<float>();
-        player.Move(motion);
+        var delta = context.ReadValue<float>();
+        _motion = new Vector3(delta, _motion.y);
+        character.Move(_motion);
+    }
+    
+    /// <summary>
+    /// Перемещает игрока в нужном направлении
+    /// </summary>
+    /// <param name="context">Содержит информацию о вводе пользователя</param>
+    public void OnVerticalMotion(InputAction.CallbackContext context)
+    {
+        var delta = context.ReadValue<float>();
+        _motion = new Vector3( _motion.x, delta);
+        character.Move(_motion);
     }
 }
