301a4030fc57ce4ec343b48435e159121ff365e4
Sizykh Nick <nksnick@gmail.com>
2022-05-03 09:47:05 +1000

    Добавил перезагрузку уровня

---
+++ Assets/Scripts/Components/EnterTriggerComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class EnterTriggerComponent : MonoBehaviour
+{
+    [SerializeField] private String objectTag;
+    [SerializeField] private UnityEvent action;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag(objectTag))
+        {
+            action.Invoke();
+        }
+    }
+}
