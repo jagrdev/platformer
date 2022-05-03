63960b96c16bec0ce02f59ac9a60af9351488320
Sizykh Nick <nksnick@gmail.com>
2022-05-03 19:28:31 +1000

    Добавил удаляемую монетку с переиспользуемым компонентом удаления

---
+++ Assets/Scripts/Components/DestroyObjectComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public class DestroyObjectComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject objectForDestroy;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            Destroy(objectForDestroy);
+        }
+    }
+}
