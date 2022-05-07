db988be6033b1a444c338a6889fb31eb7f14d3b0
Sizykh Nick <nksnick@gmail.com>
2022-05-07 16:46:11 +1000

    Добавил перемещение камеры за персонажем

---
+++ Assets/Scripts/Components/FollowTargetComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    /// <summary>
+    /// Перемещает объект к координатам цели
+    /// </summary>
+    public class FollowTargetComponent : MonoBehaviour
+    {
+        /// <summary>
+        /// Цель, за которой перемещаем текущий объект
+        /// </summary>
+        [SerializeField] private Transform target;
+
+        /// <summary>
+        /// Парметр сглаживания рывков при перемещении
+        /// </summary>
+        [SerializeField] private float damping;
+
+        private void LateUpdate()
+        {
+            var targetPosition = target.position;
+            var destination = new Vector3(targetPosition.x, targetPosition.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * damping);
+        }
+    }
+}
