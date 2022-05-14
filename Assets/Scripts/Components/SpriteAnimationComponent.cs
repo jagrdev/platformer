ded65fae8f4f4d88faa1899c6a1d7a3fc8226689
Sizykh Nick <nksnick@gmail.com>
2022-05-14 12:58:05 +1000

    Переход на обновленный скрипт анимации
    Что позволило избавиться от дочернего объекта для анимации удаления

--- Assets/Scripts/Components/SpriteAnimationComponent.cs
+++ Assets/Scripts/Components/SpriteAnimationComponent.cs
@@ -7,49 +7,115 @@ namespace Components
     [RequireComponent(typeof(SpriteRenderer))]
     public class SpriteAnimationComponent : MonoBehaviour
     {
-        [SerializeField] private Sprite[] sprites;
-        [SerializeField] private int frameRate;
-        [SerializeField] private bool loop;
-        [SerializeField] private UnityEvent onComplete;
+        [SerializeField] [Range(1, 60)] private int _frameRate = 10;
+        [SerializeField] private UnityEvent<string> _onComplete;
+        [SerializeField] private AnimationClip[] _clips;
 
-        private SpriteRenderer _spriteRenderer;
+        private SpriteRenderer _renderer;
+
         private float _secondsPerFrame;
-        private int _currentSpriteIndex;
         private float _nextFrameTime;
+        private int _currentFrame;
+        private bool _isPlaying = true;
 
-        private void Awake()
+        private int _currentClip;
+
+        private void Start()
         {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _renderer = GetComponent<SpriteRenderer>();
+            _secondsPerFrame = 1f / _frameRate;
+
+            StartAnimation();
+        }
+
+        private void OnBecameVisible()
+        {
+            enabled = _isPlaying;
+        }
+
+        private void OnBecameInvisible()
+        {
+            enabled = false;
         }
 
         private void OnEnable()
         {
-            _secondsPerFrame = 1F / frameRate;
             _nextFrameTime = Time.time + _secondsPerFrame;
-            _currentSpriteIndex = 0;
+            _currentFrame = 0;
+        }
+
+        public void SetClip(string clipName)
+        {
+            for (int i = 0; i < _clips.Length; i++)
+            {
+                if (_clips[i].Name == clipName)
+                {
+                    _currentClip = i;
+                    StartAnimation();
+                    return;
+                }
+            }
+
+            enabled = _isPlaying = false;
+        }
+
+        private void StartAnimation()
+        {
+            _nextFrameTime = Time.time + _secondsPerFrame;
+            enabled =_isPlaying = true;
+            _currentFrame = 0;
         }
 
         private void Update()
         {
             if (_nextFrameTime > Time.time) return;
 
-            if (_currentSpriteIndex >= sprites.Length)
+            var clip = _clips[_currentClip];
+
+            if (_currentFrame >= clip.Sprites.Length)
             {
-                if (loop)
+                if (clip.Loop)  
                 {
-                    _currentSpriteIndex = 0;
+                    _currentFrame = 0;
                 }
                 else
                 {
-                    enabled = false;
-                    onComplete.Invoke();
-                    return;
+                    enabled = _isPlaying = clip.AllowNextClip;
+
+
+                    clip.OnComplete?.Invoke();
+                    _onComplete?.Invoke(clip.Name);
+
+                    if (clip.AllowNextClip)
+                    {
+                        _currentFrame = 0;
+                        _currentClip = (int)Mathf.Repeat(_currentClip + 1, _clips.Length);
+                    }
                 }
+
+                return;
             }
 
-            _spriteRenderer.sprite = sprites[_currentSpriteIndex];
-            _currentSpriteIndex++;
+            _renderer.sprite = clip.Sprites[_currentFrame];
+
             _nextFrameTime += _secondsPerFrame;
+            _currentFrame++;
         }
+    }
+
+    [Serializable]
+    public class AnimationClip
+    {
+        [SerializeField] private string _name;
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private bool _loop;
+        [SerializeField] private bool _allowNextClip;
+        [SerializeField] private UnityEvent _onComplete;
+
+        public string Name => _name;
+        public Sprite[] Sprites => _sprites;
+        public bool Loop => _loop;
+        public bool AllowNextClip => _allowNextClip;
+        public UnityEvent OnComplete => _onComplete;
     }
 }
