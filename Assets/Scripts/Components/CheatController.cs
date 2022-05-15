411395cf98246957e18eaf2b428a55fe25ec9b6f
Sizykh Nick <nksnick@gmail.com>
2022-05-15 17:51:55 +1000

    Добавил использование читов

---
+++ Assets/Scripts/Components/CheatController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Components
+{
+    /// <summary>
+    /// Добавляет возможность использовать читы
+    /// </summary>
+    public class CheatController : MonoBehaviour
+    {
+        [SerializeField] private CheatCode[] _codes;
+        [SerializeField] private float _resetInputTime;
+
+        private string _cheatCode;
+        private float _estimateResetTime;
+
+        private void Awake()
+        {
+            Keyboard.current.onTextInput += OnTextInput;
+        }
+
+        private void OnTextInput(char c)
+        {
+            _estimateResetTime = _resetInputTime;
+            _cheatCode += c;
+        }
+
+        private void Update()
+        {
+            if (_cheatCode != "")
+            {
+                HandleInputCheat();
+            }
+        }
+
+        private void HandleInputCheat()
+        {
+            _estimateResetTime -= Time.deltaTime;
+            if (_estimateResetTime > 0) return;
+            
+            foreach (var cheatCode in _codes)
+            {
+                if (cheatCode.Name == _cheatCode)
+                {
+                    cheatCode.Action.Invoke();
+                }
+            }
+            _cheatCode = "";
+            _estimateResetTime = _resetInputTime;
+        }
+    }
+
+    [Serializable]
+    public class CheatCode
+    {
+        public string Name;
+        public UnityEvent Action;
+    }
+}
