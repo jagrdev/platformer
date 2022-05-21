4944178d63a04bab6c17db8f4583845ada4990a5
Sizykh Nick <nksnick@gmail.com>
2022-05-21 16:28:11 +1000

    Добавил телепорт

---
+++ Assets/Scripts/Components/TeleportComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    /// <summary>
+    /// Перемещает объект в заданные координаты
+    /// </summary>
+    public class TeleportComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _destination;
+        
+        public void Teleport(GameObject target)
+        {
+            target.transform.position = _destination.position;
+        }
+    }
+}
