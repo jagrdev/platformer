d20a416bd152bf601daa45d08bff9ea940b6ae3b
Sizykh Nick <nksnick@gmail.com>
2022-05-28 21:13:31 +1000

    Добавил компонент отображения вылетающей пыли

---
+++ Assets/Scripts/Components/SpawnComponent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    /// <summary>
+    /// Создает нужный объект в указанном месте
+    /// </summary>
+    public class SpawnComponent : MonoBehaviour
+    {
+        /// <summary>
+        /// Место создания
+        /// </summary>
+        [SerializeField] private Transform _target;
+        
+        /// <summary>
+        /// Объект для создания
+        /// </summary>
+        [SerializeField] private GameObject _prefab;
+        
+        /// <summary>
+        /// Вызов создания объекта
+        /// </summary>
+        [ContextMenu("Spawn")]
+        void Spawn()
+        {
+            Instantiate(_prefab, _target.position, Quaternion.identity);
+        }
+    }
+}
